cecca360065fb1df2e1c3977fe3438bbb4a14152
Borel KOUMO <borelkoumo@yahoo.fr>
2021-05-14 17:27:00 +0200

    Je commence le chapitre : Utilisez les Props

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Search from './Components/Search.js';
@@ -13,9 +12,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    // flexDirection (axe d'alignement des vues) et alignContent (alignement des enfants de la vue) sont deux paires qui marchent toujours ensemble
     flex: 1,
+    flexDirection : 'column', //flexDirection (axe d'alignement des vues). flexDirection controls which directions children of a container go.
+    justifyContent: 'flex-start', // Le style  justifyContent  permet de définir comment sont distribués vos components enfants sur l'axe principal. Première chose, l'axe principal dépend de l'alignement choisi avec le style  flexDirection. It defines the alignment along the main axis. It helps distribute extra free space leftover when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size.
+    alignContent: 'flex-start', // alignContent (alignement des enfants de la vue par rapport àà leur parent). alignContent controls how rows align in the cross direction, overriding the alignContent of the parent.
+    alignItems : 'center', // Le style  alignItems  est comme  justifyContent, sauf qu'il s'applique sur l'axe secondaire.
+    marginTop : 50,
+    borderColor: 'red',
+    borderWidth:2,
     backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- Components/Search.js
+++ Components/Search.js
@@ -1,32 +1,62 @@
 import React from "react"
-import {View, Button, TextInput} from "react-native"
+import {StyleSheet, View, Button, TextInput, Alert } from "react-native"
 
 class Search extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {inputValue: ""};
+      /*this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);*/
+  }
 
+  handleChange(event) {
+    this.setState({inputValue: event.target.value});
+  }
 
+  handleSubmit(event) {
+    Alert.alert('A name was submitted: ' + this.state.inputValue);
+    event.preventDefault();
+  }
+
+  //const [text, setText] = useState('');
   render () {
 
     return (
-      <View>
-        <TextInput onChange={e => {values.inputValue = e.target.value}} style={styles.textinput} autoFocus = {true} placeholder="Rechercher un film" />
-        <Button title="Rechercher" onPress = { () => { alert (values.inputValue) } }/>
+      <View style={styles.viewStyle}>
+        <TextInput onChange={e => this.handleChange(e)} style={styles.textInput}
+        autoFocus={true} placeholder="Rechercher un film"/>
+        <Button title="Rechercher" onPress = { e => { this.handleSubmit(e) } } style={styles.buttonStyle}/>
       </View>
     );
   }
 }
 
-var values = {
+const values = {
   inputValue : ""
 };
 
-const styles= {
-  textinput : {
-    margin: 20,
-    height:50,
+const styles = StyleSheet.create ({
+  viewStyle : {
+    flex : 1,
+    flexDirection : 'row',
+    alignItems : 'flex-start',
+    justifyContent: 'space-between',
     borderColor:'#c0c0c0',
     borderWidth:1,
+    padding : 5
+  },
+  textInput : {
+    flex : 1,
+    marginRight: 10,
+    height:35,
+    borderWidth:.5,
+    borderColor:'#c0c0c0',
     paddingLeft:10
+  },
+  buttonStyle : {
+    flex : 1,
+    height: 60,
   }
-}
+})
 
 export default Search
