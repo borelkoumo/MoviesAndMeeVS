d50f194fb88fea471848e13363415246339a4ba8
Borel KOUMO <borelkoumo@yahoo.fr>
2021-05-14 15:14:17 +0200

    Ici dans Search, j'ai ajouté la recupération de la valeur d'un champ texte

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c0c0c0',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Components/Search.js
+++ Components/Search.js
@@ -2,13 +2,30 @@ import React from "react"
 import {View, Button, TextInput} from "react-native"
 
 class Search extends React.Component {
+
+
   render () {
+
     return (
       <View>
-        <TextInput placeholder="Rechercher un film"/>
-        <Button title="Rechercher" onPress = { () => { alert ("Cliquera")}}/>
+        <TextInput onChange={e => {values.inputValue = e.target.value}} style={styles.textinput} autoFocus = {true} placeholder="Rechercher un film" />
+        <Button title="Rechercher" onPress = { () => { alert (values.inputValue) } }/>
       </View>
     );
+  }
+}
+
+var values = {
+  inputValue : ""
+};
+
+const styles= {
+  textinput : {
+    margin: 20,
+    height:50,
+    borderColor:'#c0c0c0',
+    borderWidth:1,
+    paddingLeft:10
   }
 }
 
